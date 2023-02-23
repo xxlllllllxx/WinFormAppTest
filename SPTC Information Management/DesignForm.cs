0a49fb73e55ec766c0b9a64b2dc6e719b588db3d
bookware pc <bookware pc@BOOKWARE>
2023-02-23 17:48:29 +0800

    pdf file

--- SPTC Information Management/DesignForm.cs
+++ SPTC Information Management/DesignForm.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Pdf;
 
 namespace SPTC_Information_Management
 {
@@ -16,5 +21,23 @@ namespace SPTC_Information_Management
         {
             InitializeComponent();
         }
-    }
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			SaveFileDialog saveFileDialog = new SaveFileDialog();
+			saveFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+			if (saveFileDialog.ShowDialog() != DialogResult.OK)
+			{
+				return;
+			}
+			//EDIT pdf here
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+			if (openFileDialog.ShowDialog() != DialogResult.OK)
+			{
+				return;
+			}
+			//PRINT pdf here
+		}
+	}
 }
